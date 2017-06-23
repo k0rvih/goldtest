30629bac2ee910ad0e6605dfc45d2a84be1c04d8
k0rvih <barbul-s@ukr.net>
2017-06-23 16:27:13 +0300

    Add files via upload

---
+++ goldtest.ps1
@@ -0,0 +1,14 @@
+Configuration goldtest
+{
+  param ($MachineName)
+
+  Node $MachineName
+  {
+    #Install the IIS Role
+    WindowsFeature IIS
+    {
+      Ensure = “Present”
+      Name = “Web-Server”
+    }
+  }
+} 
